fd4bf00f0563ce664153e9446147d6c8457d0d02
ryanglennsmith <ryan.glenn.smith@gmail.com>
2022-02-09 12:51:55 +0000

    functional changes to handle values on btns and square state

--- src/components/Board.js
+++ src/components/Board.js
@@ -15,28 +15,34 @@ const initialSquaresState = [
 const Board = () => {
   const [square, setSquare] = useState(initialSquaresState);
 
-  const handleClick = () => {
-    console.log("click");
+  const handleClick = (e) => {
+    let newSquare = [...square];
+    newSquare[e.target.value] = "player";
+    setSquare(newSquare);
+    console.log(`click value: ${e.target.value} and square: ${square}`);
   };
-
+  console.log(square);
   return (
     <div>
       <div style={{ display: "flex" }}>
-        <Square squareState={square} handleClick={handleClick} />
-        | <Square squareState={square} handleClick={handleClick} /> |{" "}
-        <Square squareState={square} handleClick={handleClick} />{" "}
+        <Square squareState={square} handleClick={handleClick} value={0} />
+        | <Square
+          squareState={square}
+          handleClick={handleClick}
+          value={1}
+        /> | <Square squareState={square} handleClick={handleClick} value={2} />{" "}
       </div>
       <br />
       <div style={{ display: "flex" }}>
-        <Square squareState={square} handleClick={handleClick} />|{" "}
-        <Square squareState={square} handleClick={handleClick} />|{" "}
-        <Square squareState={square} handleClick={handleClick} />
+        <Square squareState={square} handleClick={handleClick} value={3} />|{" "}
+        <Square squareState={square} handleClick={handleClick} value={4} />|{" "}
+        <Square squareState={square} handleClick={handleClick} value={5} />
       </div>
       <br />
       <div style={{ display: "flex" }}>
-        <Square squareState={square} handleClick={handleClick} /> |{" "}
-        <Square squareState={square} handleClick={handleClick} />|{" "}
-        <Square squareState={square} handleClick={handleClick} />
+        <Square squareState={square} handleClick={handleClick} value={6} /> |{" "}
+        <Square squareState={square} handleClick={handleClick} value={7} />|{" "}
+        <Square squareState={square} handleClick={handleClick} value={8} />
       </div>
     </div>
   );
